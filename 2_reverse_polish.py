5be2c25ba88d010f06824d3eb795e3502fce0667
Praneeth Arra <parra@mtu.edu>
2016-09-15 01:26:07 -0400

    Create 2_reverse_polish.py

---
+++ 2_reverse_polish.py
@@ -0,0 +1,44 @@
+def reverse_polish(in_list):
+    res = 0
+    i = 0
+    while i < len(in_list):
+        if in_list[i] in ['+','-', '*', '/']:
+            if in_list[i] == '+':
+                ind_op = in_list.index(in_list[i])
+                res = float(in_list[ind_op-2]) + float(in_list[ind_op-1])
+                in_list[ind_op-2] = res
+                i = ind_op-2 
+                del in_list[ind_op]
+                del in_list[ind_op-1]
+                
+            elif in_list[i] == '-':
+                ind_op = in_list.index(in_list[i])
+                res = float(in_list[ind_op-2]) - float(in_list[ind_op-1])
+                in_list[ind_op-2] = res
+                i = ind_op-2 
+                del in_list[ind_op]
+                del in_list[ind_op-1]
+                
+            elif in_list[i] == '*':
+                ind_op = in_list.index(in_list[i])
+                res = float(in_list[ind_op-2]) * float(in_list[ind_op-1])
+                in_list[ind_op-2] = res
+                i = ind_op-2 
+                del in_list[ind_op]
+                del in_list[ind_op-1]
+                
+            else:
+                ind_op = in_list.index(in_list[i])
+                res = float(in_list[ind_op-2]) / float(in_list[ind_op-1])
+                in_list[ind_op-2] = res
+                i = ind_op-2 
+                del in_list[ind_op]
+                del in_list[ind_op-1]
+        else:
+            i = i+1
+                
+    return in_list[0]
+    
+
+a = ["4", "13", "5", "/", "+"]
+reverse_polish(a)
